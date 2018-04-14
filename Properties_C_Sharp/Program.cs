736c686b5cad9a2eca16c2d0fd3eb4c517f1aedd
bhargavak <bhargavak37@gmail.com>
2018-04-14 18:43:59 +0530

    Add project files.

---
+++ Properties_C_Sharp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.Title = "Bossypants";
+            string bookTitle = book.Title;
+
+        }
+    }
+    public class Book
+    {
+        private string _title;
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+            }
+        }
+    }
+    
+}
